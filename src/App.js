a788d81fcc1ed3235fd417035b7abe6a4abc057c
Dahbi-Dev <houssamdahbi007@gmail.com>
2024-04-16 23:31:42 +0100

    add close functionality

--- src/App.js
+++ src/App.js
@@ -8,42 +8,46 @@ const messages = [
 
 function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true)
 
   function nextClick() {
-    setStep(step + 1);
-    if (step >= 3) setStep(3);
+    if (step < 3) setStep(step + 1);
   }
   function previousClick() {
-    setStep(step - 1);
-    if (step <= 1) setStep(1);
+    if (step > 1) setStep(step - 1);
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
+    <div>
+      <button className="close" onClick={()=>setIsOpen(!isOpen)}>&times;</button>
 
-      <p className="message">{`step ${step} : ${messages[step - 1]}`}</p>
+     { isOpen && (
 
-      <div className="buttons">
-        <button
-          className="previous"
-          style={{ backgroundColor: "#7950f2", color: "white" }}
-          onClick={previousClick}
-        >
-          Previous
-        </button>
-        <button
-          className="next"
-          style={{ backgroundColor: "#7950f2", color: "white" }}
-          onClick={nextClick}
-        >
-          Next
-        </button>
-      </div>
+   
+     <div className="steps">
+        <div className="numbers">
+          <div className={step >= 1 ? "active" : ""}>1</div>
+          <div className={step >= 2 ? "active" : ""}>2</div>
+          <div className={step >= 3 ? "active" : ""}>3</div>
+        </div>
+        <p className="message">{`step ${step} : ${messages[step - 1]}`}</p>
+        <div className="buttons">
+          <button
+            className="previous"
+            style={{ backgroundColor: "#7950f2", color: "white" }}
+            onClick={previousClick}
+          >
+            Previous
+          </button>
+          <button
+            className="next"
+            style={{ backgroundColor: "#7950f2", color: "white" }}
+            onClick={nextClick}
+          >
+            Next
+          </button>
+        </div>
+      </div>)}
     </div>
   );
 }
